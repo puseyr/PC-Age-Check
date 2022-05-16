2b42dc6cdb7b75db277942c4a02f8cfe8fb2644a
puseyr <pusey.ronald@gmail.com>
2022-05-15 21:46:18 -0400

    Adding PS1 Script

---
+++ prototype2ps1.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module ActiveDirectory
+
+$computers = Get-Content '\\usnycsrv2uwfp1\upf\rpusey-sa\WF\RF\Documents\PowerShell building\PC Age\dedcompoopers.txt'
+
+foreach ($computer in $computers) {
+    if (Test-Connection $computer -Count 2 -Quiet) {
+        Get-ADComputer -Identity $computer -Properties * | Select cn, created, lastlogondate | Export-Csv "\\usnycsrv2uwfp1\upf\rpusey-sa\WF\RF\Documents\PowerShell building\PC Age\DedCompoopsList.csv" -NoTypeInformation -Append
+    }
+
+    else{
+        Get-ADComputer -Filter -Identity $_ -Properties * | Select cn, created, lastlogondate | Export-Csv "\\usnycsrv2uwfp1\upf\rpusey-sa\WF\RF\Documents\PowerShell building\PC Age\DedCompoopsList.csv" -NoTypeInformation -Append
+    }
+}
